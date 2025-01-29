53c7fbbc734051e0dec942837591c79704ab78bf
Joshua-Willey <joshuawilley88@gmail.com>
2025-01-29 04:49:26 +0200

    add: added Cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 const Cart = () => {
   const state = useSelector((state) => state.handleCart);
   const dispatch = useDispatch();
+  var temp = 0;
 
   const EmptyCart = () => {
     return (
@@ -129,7 +130,8 @@ const Cart = () => {
                   <div className="card-body">
                     <ul className="list-group list-group-flush">
                       <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                        Products ({totalItems})<span>${Math.round(subtotal)}</span>
+                        Products ({totalItems})
+                        <span>${Math.round(subtotal)}</span>
                       </li>
                       <li className="list-group-item d-flex justify-content-between align-items-center px-0">
                         Shipping
